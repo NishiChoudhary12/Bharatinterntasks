01cce6396824d59e96f6f30ab3f7624729d3079b
Nishichoudhary <133603881+NishiChoudhary12@users.noreply.github.com>
2023-06-10 14:48:48 +0530

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,29 @@
+import numpy as np 
+
+# data processing
+import pandas as pd 
+
+# data visualization
+import seaborn as sns
+%matplotlib inline
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+# Algorithms
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+
+test_df = pd.read_csv("test.csv")
+train_df = pd.read_csv("train.csv")
+
+print(test_df)
+train_df.info()
+train_df.describe()
+train_df.head(8)
